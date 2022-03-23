88f9ed89beb7a5500114447e9b18a08b753de1af
ronderksen <ron.derksen@trivento.nl>
2022-03-23 16:24:38 +0100

    feature: add login and registration components

---
+++ src/hooks/use-form-handler.ts
@@ -0,0 +1,59 @@
+import * as yup from "yup";
+import * as React from "react";
+
+export function useFormHandler<Values extends {}>(
+    schema: yup.AnySchema,
+    submitHandler: (values: Values) => void
+) {
+    type Errors = { [FieldName in keyof Values]: string; };
+
+    const [values, setValues] = React.useState<Values>({} as Values);
+    const [errors, setErrors] = React.useState<Errors>({} as Errors);
+    const [isSubmitting, setSubmitting] = React.useState(false);
+
+    const hasError = (fieldName: keyof Errors) => errors[fieldName] && errors[fieldName] !== '';
+
+    const onFieldChange = React.useCallback((event: React.FormEvent<HTMLInputElement>) => {
+        const fieldName = event.currentTarget.name;
+        const value = event.currentTarget.value;
+        setValues((prevValues: Values) => ({
+                ...prevValues,
+                [fieldName]: value
+            })
+        )
+    }, []);
+
+    const onSubmit = React.useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        schema.validate(values, {abortEarly: false})
+            .then((isFormValid) => {
+                if (isFormValid) {
+                    setSubmitting(true);
+                    submitHandler(values);
+                    setSubmitting(false);
+                }
+            })
+            .catch((err) => {
+            const errors = err.inner.reduce((acc: Record<string, boolean>, error: yup.ValidationError) => {
+                return error.path
+                    ? {
+                        ...acc,
+                        [error.path]: error.message,
+                    }
+                    : acc
+            }, {})
+
+            // Update form errors state:
+            setErrors(errors);
+        })
+    }, [schema, values, submitHandler])
+
+    return {
+        values,
+        errors,
+        hasError,
+        onFieldChange,
+        onSubmit,
+        isSubmitting
+    }
+}
